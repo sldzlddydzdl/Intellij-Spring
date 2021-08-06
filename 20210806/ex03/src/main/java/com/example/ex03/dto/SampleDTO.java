579baacb7d7db74c8839c9f7912ba02ee498c632
sldzlddydzdl <71972556+sldzlddydzdl@users.noreply.github.com>
2021-08-06 23:30:55 +0900

    Add files via upload

---
+++ 20210806/ex03/src/main/java/com/example/ex03/dto/SampleDTO.java
@@ -0,0 +1,20 @@
+package com.example.ex03.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder(toBuilder = true)
+public class SampleDTO {
+
+    private Long sno;
+
+    private String first;
+
+    private String last;
+
+    private LocalDateTime regTime;
+
+}
