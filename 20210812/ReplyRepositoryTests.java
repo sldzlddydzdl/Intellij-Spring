51075d60269310a6896899146cae35d38a1b38e7
sldzlddydzdl <71972556+sldzlddydzdl@users.noreply.github.com>
2021-08-12 23:17:04 +0900

    Add files via upload

---
+++ 20210812/ReplyRepositoryTests.java
@@ -0,0 +1,37 @@
+package com.example.board.repository;
+
+import com.example.board.entity.Board;
+import com.example.board.entity.Member;
+import com.example.board.entity.Reply;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.stream.IntStream;
+
+@SpringBootTest
+public class ReplyRepositoryTests {
+
+    @Autowired
+    private ReplyRepository replyRepository;
+
+    @Test
+    public void insertReply(){
+        IntStream.rangeClosed(1, 300).forEach(i ->{
+            long bno = (long)(Math.random()*100) + 1;
+
+            Board board = Board.builder()
+                    .bno(bno)
+                    .build();
+
+            Reply reply = Reply.builder()
+                    .text("Reply......" + i)
+                    .board(board)
+                    .replyer("guest")
+                    .build();
+
+            replyRepository.save(reply);
+
+        });
+    }
+}
